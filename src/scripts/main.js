10ef7d9d38d3a0ed325c2090bf3ee90f976cd7a0
Tim Lane <timlanesoftware@gmail.com>
2020-12-16 13:00:36 +0000

    Reddit JSON GET request

---
+++ src/scripts/main.js
@@ -0,0 +1,32 @@
+function main(tab) {
+  let mainurl = tab.url + '.json';
+
+  var xhttp = new XMLHttpRequest();
+
+  xhttp.open("GET", mainurl, true);
+  xhttp.setRequestHeader("Content-Type", "*/*");
+
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4) {
+      if (xhttp.status == 200) {
+        parseJSON(xhttp.responseText);
+      } else {
+        parseJSON(null);
+      }
+    }
+  };
+  xhttp.send();
+}
+
+function parseJSON(data) {
+  if (data != null)
+  {
+    chrome.tabs.create({url: 'src/ui/output.html'});
+  }
+  else
+  {
+    chrome.tabs.create({url: 'src/ui/error.html'});
+  }
+}
+
+chrome.browserAction.onClicked.addListener(main);
