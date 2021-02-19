176f64284608f53e2ec826f6056665dfc4ac80ad
Tim Lane <timlanesoftware@gmail.com>
2021-02-19 16:38:10 +0000

    URL extraction from main post

--- src/scripts/process_json.js
+++ src/scripts/process_json.js
@@ -1,12 +1,3 @@
-/*try {
-    process.argv.forEach((val, index) => {
-        console.log(`${index}: ${val}`);
-    });
-}
-catch (err) {
-    //console.log("Error: " + err.message)
-    alert("Error: " + err.message);
-}*/
 
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
@@ -31,7 +22,54 @@ function download_raw(url, parseDataCallback) {
 }
 
 function process_raw(raw_json) {
-    data = raw_json;
+    var data = JSON.parse(raw_json);
+    
+    var postLinks = [];
+    
+    if (data.length > 0) {
+        // First, analyse the post itself        
+        for (var i = 0, counti = data[0].data.children.length; i < counti; i++) {
+            post = data[0].data.children[i].data.selftext;
+            
+            var foundIndex = 0;
+            do {
+                foundIndex = post.indexOf("://", foundIndex);
+                if (foundIndex >= 0) {
+                    // First make sure link is http or https
+                    if (foundIndex >= 4 && post.slice(foundIndex - 4, foundIndex) === "http" ||
+                        foundIndex > 4 && post.slice(foundIndex - 5, foundIndex) === "https") {
+                        
+                        var url = "http";
+                        var allowedCharacters = "!#$&'()*+,/:;=?@[]abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_.~%";
+                        for (var c = foundIndex; c < post.length; c++) {
+                            
+                            var cc = post[c].charCodeAt(0);
+                            var isValid = (cc === 33 || (cc >= 35 && cc <= 59) ||
+                            cc === 61 || (cc >= 63 && cc <= 91) ||
+                            cc === 93 || cc === 95 || 
+                            (cc >= 97 && cc <= 122) || cc === 126);
+                                                        
+                            if (!isValid) {
+                                break;
+                            }
+                            
+                            url = url + String(post[c]);
+                        }
+                        
+                        postLinks.push(url);
+                    }
+                    foundIndex += 1;
+                }
+            } while (foundIndex >= 0 && foundIndex < post.length);
+            
+            
+        }
+        
+        console.log("Found links:\n\n" + String(postLinks));
+        
+    }
+    
+    
     return data;
 }
 
