48b7410b39ea964f578d45774c831a3fe30cfb4a
Tim Lane <timlanesoftware@gmail.com>
2021-02-19 17:22:43 +0000

    Implemented pushshift search for links

--- src/scripts/process_json.js
+++ src/scripts/process_json.js
@@ -36,10 +36,14 @@ function process_raw(raw_json) {
                 foundIndex = post.indexOf("://", foundIndex);
                 if (foundIndex >= 0) {
                     // First make sure link is http or https
-                    if (foundIndex >= 4 && post.slice(foundIndex - 4, foundIndex) === "http" ||
-                        foundIndex > 4 && post.slice(foundIndex - 5, foundIndex) === "https") {
+                    var isSecure = foundIndex > 4 && post.slice(foundIndex - 5, foundIndex) === "https";
+                    if (foundIndex >= 4 && post.slice(foundIndex - 4, foundIndex) === "http" || isSecure) {
                         
-                        var url = "http";
+                        // TODO: make sure foundindex - 5 or - 6 is valid
+                        var hasOpenBracket = post[foundIndex - (isSecure ? 6 : 5)] == '(';
+                        var numOpenBrackets = hasOpenBracket ? 1 : 0;
+                        
+                        var url = isSecure ? "https" : "http";
                         var allowedCharacters = "!#$&'()*+,/:;=?@[]abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_.~%";
                         for (var c = foundIndex; c < post.length; c++) {
                             
@@ -48,7 +52,19 @@ function process_raw(raw_json) {
                             cc === 61 || (cc >= 63 && cc <= 91) ||
                             cc === 93 || cc === 95 || 
                             (cc >= 97 && cc <= 122) || cc === 126);
-                                                        
+                            
+                            if (cc == 40) {
+                                numOpenBrackets++;
+                            }
+                            if (cc == 41)
+                            {
+                                numOpenBrackets--;
+                                if (numOpenBrackets <= 0 && hasOpenBracket)
+                                {
+                                    isValid = false;
+                                }
+                            }
+                            
                             if (!isValid) {
                                 break;
                             }
@@ -65,7 +81,36 @@ function process_raw(raw_json) {
             
         }
         
-        console.log("Found links:\n\n" + String(postLinks));
+        console.log("Found links:\n" + String(postLinks));
+
+        var query = "http://api.pushshift.io/reddit/submission/search/?q="
+        
+        // Now convert links to searchable URI strings and search with pushshift
+        var occurrenceCount = 0;
+        for (var i = 0, counti = postLinks.length; i < counti; i++) {
+            postLinks[i] = encodeURIComponent(postLinks[i]);
+            console.log("\nSearching Reddit for occurrence of link:\n" + postLinks[i]);
+            
+            var xhttp = new XMLHttpRequest();
+
+            xhttp.open("GET", query + postLinks[i], false);
+            xhttp.setRequestHeader("Content-Type", "*/*");
+
+            xhttp.onreadystatechange = function() {
+                if (xhttp.readyState == 4) {
+                    if (xhttp.status == 200) {
+                        if (xhttp.responseText != null) {
+                            results = JSON.parse(xhttp.responseText);
+                            //console.log(results.data[0].full_link);
+                            occurrenceCount++;
+                        }
+                    }
+                }
+            };
+            xhttp.send();
+            
+        }
+        
         
     }
     
