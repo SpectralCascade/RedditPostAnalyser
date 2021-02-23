054960c489e45c208a48c27f237278f188c6997b
Tim Lane <timlanesoftware@gmail.com>
2021-02-23 12:43:29 +0000

    Fixed graphs data

--- src/ui/doughnut.js
+++ src/ui/doughnut.js
@@ -1,11 +1,11 @@
 var data = localStorage.getItem("redditDataJSON");
 
 // Parse the JSON data
-var obj = JSON.parse(data);
+var processed = JSON.parse(data);
 
 var label = ["Upvotes","Downvotes"]
-var upvotes = obj[0].data.children[0].data.ups;
-var downvotes = Math.round(((totalUps / (obj[0].data.children[0].data.upvote_ratio * 100)) * 100) - totalUps);
+var upvotes = processed.upVotes;
+var downvotes = processed.downEst;
 var data = [upvotes,downvotes];
 
 var ctx = document.getElementById("doughnut");
--- src/ui/pie.js
+++ src/ui/pie.js
@@ -1,8 +1,8 @@
 var data = localStorage.getItem("redditDataJSON");
-var obj = JSON.parse(data);
+var processed = JSON.parse(data);
 var label = ["Upvotes","Downvotes"]
-var upvotes = obj[0].data.children[0].data.ups;
-var downvotes = Math.round(((totalUps / (obj[0].data.children[0].data.upvote_ratio * 100)) * 100) - totalUps);
+var upvotes = processed.upVotes;
+var downvotes = processed.downEst;
 var data = [upvotes,downvotes];
 
 var ctx = document.getElementById("pie");
