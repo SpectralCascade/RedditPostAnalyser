8d9c6e31190520210d276d5e0507f5b10b81280f
Tim Lane <timlanesoftware@gmail.com>
2021-02-27 12:52:58 +0000

    Fixed modal popup, WIP styling

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,21 +1,23 @@
+var mainTab = null;
 
 function parseJSON(data) {
-  if (data != null)
-  {
-    data = process_raw(data);
-    localStorage.setItem("redditDataJSON", data);
-    chrome.tabs.create({url: 'src/ui/output.html'});
-  }
-  else
-  {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {action: "Popup"}, function(response) {
-      });
-    });
-  }
+    if (data != null)
+    {
+        data = process_raw(data);
+        localStorage.setItem("redditDataJSON", data);
+        chrome.tabs.create({url: 'src/ui/output.html'});
+    }
+    else if (mainTab != null)
+    {
+        chrome.tabs.sendMessage(mainTab.id, {action: "Popup"}, function(response) {
+            // Nothing here needed
+        });
+    }
+    mainTab = null;
 }
 
 function main(tab) {
+    mainTab = tab;
     download_raw(tab.url, parseJSON);
 }
 
--- src/ui/popup.js
+++ src/ui/popup.js
@@ -1,10 +1,8 @@
 function Popup() {
+    var iFrame  = document.createElement ("iframe");
+    iFrame.src  = chrome.extension.getURL ("src/ui/modal.html");
 
-var iFrame  = document.createElement ("iframe");
-iFrame.src  = chrome.extension.getURL ("modal.html");
-
-document.body.insertBefore (iFrame, document.body.firstChild);
-
+    document.body.insertBefore (iFrame, document.body.firstChild);
 }
 
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
