332b512210c2e9a6c9169a7f8299185c2eda53e4
Tim Lane <timlanesoftware@gmail.com>
2021-02-27 13:51:58 +0000

    Made the popup look pretty

--- src/ui/popup.js
+++ src/ui/popup.js
@@ -1,6 +1,20 @@
 function Popup() {
-    var iFrame  = document.createElement ("iframe");
+    var iFrame  = document.getElementById("_rpa_modal_popup_frame");
+    if (iFrame == null) {
+        iFrame = document.createElement ("iframe");
+        iFrame.id = "_rpa_modal_popup_frame";
+    }
+    
     iFrame.src  = chrome.extension.getURL ("src/ui/modal.html");
+    iFrame.width = 500;
+    iFrame.height = 200;
+    
+    // Make sure the popup sits on top of the page in a fixed position
+    iFrame.style.position = "fixed";
+    iFrame.style.zIndex = 9999999;
+    iFrame.style.display = "Block";
+    iFrame.style.right = 0;
+    iFrame.style.borderWidth = 0;
 
     document.body.insertBefore (iFrame, document.body.firstChild);
 }
