8456a5b7ece39695bec61641a4db5016aba96a51
Tim Lane <timlanesoftware@gmail.com>
2021-03-12 11:03:44 +0000

    Got line chart working as a module

--- src/ui/line.js
+++ src/ui/line.js
@@ -3,38 +3,49 @@ import {Infographic} from './infographic.js';
 class LineChart extends Infographic
 {
     constructor() {
-        context = document.getElementById("line");
-        chart = new Chart(context, {
+        super();
+        
+        // Our labels along the x-axis
+        this.years = [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050];
+        // For drawing the lines
+        this.africa = [86,114,106,106,107,111,133,221,783,2478];
+        this.asia = [282,350,411,502,635,809,947,1402,3700,5267];
+        this.europe = [168,170,178,190,203,276,408,547,675,734];
+        this.latinAmerica = [40,20,10,16,24,38,74,167,508,784];
+        this.northAmerica = [6,3,2,2,7,26,82,172,312,433];
+        
+        this.context = document.getElementById("line");
+        this.chart = new Chart(this.context, {
           type: 'line',
           data: {
-            labels: years,
+            labels: this.years,
             datasets: [
               {
-                data: africa,
+                data: this.africa,
                 label: "Africa",
                 borderColor: "red",
                 fill: false
               },
               {
-                data: asia,
+                data: this.asia,
                 label: "Asia",
                 borderColor: "green",
                 fill: false
               },
               {
-                data: europe,
+                data: this.europe,
                 label: "Europe",
                 borderColor: "blue",
                 fill: false
               },
               {
-                data: latinAmerica,
+                data: this.latinAmerica,
                 label: "Latin America",
                 borderColor: "orange",
                 fill: false
               },
               {
-                data: northAmerica,
+                data: this.northAmerica,
                 label: "North America",
                 borderColor: "#3e95cd",
                 fill: false
@@ -43,18 +54,6 @@ class LineChart extends Infographic
           }
         });
     }
-    
-    // Our labels along the x-axis
-    years = [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050];
-    // For drawing the lines
-    africa = [86,114,106,106,107,111,133,221,783,2478];
-    asia = [282,350,411,502,635,809,947,1402,3700,5267];
-    europe = [168,170,178,190,203,276,408,547,675,734];
-    latinAmerica = [40,20,10,16,24,38,74,167,508,784];
-    northAmerica = [6,3,2,2,7,26,82,172,312,433];
-
-    context = null;
-    chart = null;
 
 }
 
