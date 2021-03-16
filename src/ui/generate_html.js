8a780832cd2dd812e72353cfe69d791a4db16564
TomAlfano <tom.ah10@gmail.com>
2021-03-16 13:04:37 +0000

    Updates to css and scripts for css

--- src/ui/generate_html.js
+++ src/ui/generate_html.js
@@ -1,13 +1,6 @@
 var data = localStorage.getItem("redditDataJSON");
-//var duplicate = localStorage.getItem("raw_duplicate");
-//var holder = localStorage.getItem("test1");
-//var holder2 = localStorage.getItem("test2");
-//holder = JSON.parse(holder);
-// Parse the JSON data
 var processed = JSON.parse(data);
-//var duplicate_processed = JSON.parse(duplicate);
 var date = new Date();
-//document.write("<p>" + holder2.url + "</p>");    //For displaying raw JSON for counting brackets
 
 //for (i =0; i < processed.duplicates.url.length; i++){
 //  document.write("<p>" + processed.duplicates.url[i] + "</p>");    //For displaying JSON
@@ -17,6 +10,7 @@ var date = new Date();
 //document.write("<h1>Subreddit: " + obj[0].data.children[0].data.subreddit + "</h1>");
 document.write("<p style=\"font-size: 24pt;\">Analysis of <strong>" + processed.title + "</strong></p>");
 document.write("<h3>Generated on " + date + "</h3>");
+document.write("<div id = \"info\">")
 document.write("<h3>Post Uploaded on " + processed.postDate + "</h3>");
 document.write("<h3>Number of Comments: " + processed.numComments + "</h3>");
 document.write("<h3>Total Awards: " + processed.totalAwards + "</h3>");
@@ -24,6 +18,7 @@ document.write("<h3>Number of CrossPosts:  " + processed.crossPosts + "</h3>");
 document.write("<br /><h1>Upvotes: " + processed.upVotes + "</h1>");
 //var totalUps = obj[0].data.children[0].data.ups;
 document.write("<br /><h1>Downvotes (estimate): " + processed.downEst + "</h1>");
+document.write("</div>")
 document.write("<br /><h1>Comments Controversiality: " + String(processed.contCount)+ "</h1>");
 
 document.write("<br /><h3>Total links in post: " + processed.postLinks.length + "</h3>");
@@ -33,5 +28,3 @@ for (var i = 0; i < processed.postLinks.length; i++) {
     document.write("Occurrences: " + String(processed.postLinks[i].occurrences));
     document.write("Total subreddits: " + String(processed.postLinks[i].numSubreddits) + "<br /><br />");
 }
-
-//document.write("<p style=\"text-align: left;\">" + data + "</p>");
--- src/ui/infographic.js
+++ src/ui/infographic.js
@@ -1,52 +1,59 @@
 
 export class Infographic {
-    
+
     constructor(chart_id, chartType, title, width, height, data) {
         if (constructor === 'Infographic') {
             throw new Error("Cannot instantiate abstract class \"Infographic\".");
         }
-        
+
         this.context = null;
         this.chart = null;
         this.updateAnimation = false;
-        
+
         this.typename = chartType;
         this.data = data;
         this.dropdowns = [];
-        
+
         // Canvas generation
-        var allCharts = document.getElementById("all_charts");
+        if (chart_id === "donut"){
+          var allCharts = document.getElementById("donut_chart");
+        } else if (chart_id === "pie"){
+          var allCharts = document.getElementById("pie_chart");
+        } else {
+          var allCharts = document.getElementById("line_chart");
+        }
+
         var container = document.createElement("div");
         container.class = "container";
         container.style = "max-width: 800px; margin: 50px auto;";
-        
+
         container.innerHTML =
-        "<div>\n" + 
+        "<div>\n" +
         "<h2 style=\"font-size: 24pt\">" + title + "</h2>\n" +
         "<canvas id=\"" + chart_id + "\" width=\"" + width + "\" height=\"" + height + "\"></canvas>" +
         "</div>";
-        
+
         // Dropdown generation for charts with more than one option.
         if (this.data.length > 1) {
             var dropdown = document.createElement("label");
             dropdown.innerHTML = "Choose data type:\n";
             allCharts.appendChild(dropdown);
-            
+
             var selector = document.createElement("select");
-            
-            // TODO: are these needed?
+
+            // TODO: are these needed? (don't think they need to be these names, might not be needed at all, originally for testing - Tom)
             selector.class = "charts";
             selector.name = "ice-cream";
             selector.id = "chartSelect";
-            
+
             for (var i = 0; i < this.data.length; i++) {
                 selector.innerHTML = selector.innerHTML + "<option value=\"" + this.data[i].name + "\">" + this.data[i].name + "</option>\n";
             }
-            
+
             var infographic = this;
             selector.onchange = function(change_event) {
                 var selected = change_event.target.value;
-                
+
                 for (var i = 0; i < infographic.data.length; i++) {
                     if (selected === infographic.data[i].name){
                         if (infographic.chart != null) {
@@ -57,19 +64,19 @@ export class Infographic {
                         break;
                     }
                 }
-                
+
             };
-            
+
             dropdown.appendChild(selector);
         }
-        
+
         allCharts.appendChild(container);
         this.context = document.getElementById(chart_id);
 
         // Populate with default data
         this.populate(0);
     }
-    
+
     populate(index) {
         if (this.data.length > 0 && this.data[index].datasets != null && this.data[index].datasets.length > 0) {
             this.stopDrawing();
@@ -84,11 +91,11 @@ export class Infographic {
             this.startDrawing();
         }
     }
-    
+
     add(data) {
         this.data.push(data);
     }
-    
+
     startDrawing() {
         // TODO: Check if data is being loaded. If so, show spinning wheel; otherwise show "No data available".
         var canvas = this.context.getContext("2d");
@@ -97,11 +104,11 @@ export class Infographic {
         var infographic = this;
         window.requestAnimationFrame(function () { infographic.drawLoading(); });
     }
-    
+
     stopDrawing() {
         this.updateAnimation = false;
     }
-    
+
     drawLoading() {
         var canvas = this.context.getContext("2d");
         var halfDim = { w: this.context.width / 2, h: this.context.height / 2 };
@@ -111,11 +118,11 @@ export class Infographic {
         canvas.fillStyle = 'rgba(0, 255, 255, 1)';
         canvas.strokeStyle = 'rgba(200, 200, 200, 1)';
         canvas.save();
-        
+
         var loadingSpinSpeed = 1;
         var loadingDotRadius = 8;
         var loadingWheelRadius = 32;
-        
+
         // Loading wheel
         var time = (new Date()).getMilliseconds();
         var deltaTime = (time - this.lastTime) / 1000;
@@ -124,18 +131,18 @@ export class Infographic {
                 x: halfDim.w - loadingWheelRadius - loadingDotRadius,
                 y: halfDim.h - loadingWheelRadius - loadingDotRadius
             };
-            
-            
+
+
             canvas.translate(trans.x, trans.y);
             canvas.rotate((2 * Math.PI) * deltaTime * loadingSpinSpeed);
-            
+
             // Draw spinny dot
             canvas.beginPath();
             canvas.arc(loadingWheelRadius, 0, loadingDotRadius, 0, 2 * Math.PI);
             canvas.fill();
             canvas.translate(-trans.x, -trans.y);
-            
-            
+
+
             // Draw wheel
             canvas.beginPath();
             canvas.arc(trans.x, trans.y, loadingWheelRadius, 0, 2 * Math.PI);
@@ -143,7 +150,7 @@ export class Infographic {
             canvas.stroke();
         }
         this.lastTime = time;
-        
+
         if (this.updateAnimation) {
             var infographic = this;
             window.requestAnimationFrame(function () { infographic.drawLoading(); });
@@ -151,13 +158,13 @@ export class Infographic {
             canvas.restore();
         }
     }
-    
+
     drawEmpty() {
         var canvas = this.context.getContext("2d");
     }
-    
+
     update() {
         // todo, populate dropdowns etc.
     }
-    
+
 }
