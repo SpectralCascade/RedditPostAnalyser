03eb95b10457876621d975478bf6ad57db2e6afe
TomAlfano <tomalfanohughes@gmail.com>
2021-04-12 10:19:11 +0100

    Think the comments are displayed in the chart Tim wanted? maybe, hard to test if it works properly but looks like it works

--- src/ui/line.js
+++ src/ui/line.js
@@ -20,13 +20,24 @@ sortingArray.sort(function compare(a, b) {
   return a[0] - b[0];
 });
 
-//Puts sorted array into correct data format for displaying on chart
+//set up variables for calculating axis data
+//comments chart var
 var controData = [];
 var comments = [];
 var controTotal = 0;
 var sumTotal = 0;
+//incident var
+var incidentSum = 0;
+var incidentContro = 0;
+var incidentData = [];
+var incidentControData = [];
+//get first point time in ms, 5 mins = 300000ms
+var incidentStepCounter = sortingArray[0][0] + 300000;
+
+//Puts sorted array into correct data format for displaying on chart
 for (var i = 0; i <sortingArray.length; i++){
   sumTotal++;
+  // 't' is x-axis but also t for time, y for y-axis
   if (i === 0){
     controData.push({t: (new Date(sortingArray[i][0])), y: controTotal});
   } else if (sortingArray[i][1] === true){
@@ -36,6 +47,22 @@ for (var i = 0; i <sortingArray.length; i++){
     controData.push({t: (new Date(sortingArray[i][0])), y: controTotal});
   };
   comments.push({t: (new Date(sortingArray[i][0])), y: sumTotal});
+
+  //start of incident data calculation, sum every 5 mins
+  if (sortingArray[i][0] >= incidentStepCounter){
+    incidentData.push({t: (new Date((incidentStepCounter - 150000))), y: incidentSum});
+    incidentControData.push({t: (new Date((incidentStepCounter - 150000))), y: incidentContro});
+    incidentStepCounter = incidentStepCounter + 300000;
+    incidentSum = 0;
+    incidentContro = 0;
+  };
+  incidentSum++;
+  if (sortingArray[i][1] === true){
+    incidentContro++;
+  };
+  if (sortingArray[i][0] < incidentStepCounter){
+    incidentSum++;
+  }
 }
 
 var line = new LineChart(
@@ -129,7 +156,48 @@ var line = new LineChart(
               }
             }
           },
-
+          {
+              name: "Incident Comments",
+              datasets:  [{
+                label: 'Controversial Comments',
+                borderColor: 'red',
+                fill: false,
+                data: incidentControData
+              },
+              {
+                label: 'Total Comments',
+                borderColor: 'green',
+                fill: false,
+                data: incidentData
+              }],
+              options: {
+                scales: {
+                  xAxes: [{
+                    type: 'time',
+                    time: {
+                      unit: 'minute',
+                      unitStepSize: 30,
+                    },
+                    distribution: 'linear',
+                    scaleLabel: {
+                      display: true,
+                      labelString: 'Rolling 5 min Comment Post Time'
+                    }
+                  }],
+                  yAxes: [{
+                    scaleLabel: {
+                      display: true,
+                      labelString: 'Total Comments per 5 mins'
+                    }
+                  }]
+                },
+                elements: {
+                  point:{
+                    radius: 0
+                  }
+                }
+              }
+            },
     ]
 );
 
