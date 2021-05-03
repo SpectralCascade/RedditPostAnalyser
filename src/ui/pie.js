92a35e416ab00df42dce75dfdce513076b5dfcca
Tim Lane <timlanesoftware@gmail.com>
2021-05-03 17:46:37 +0100

    Show pie chart even on 0 upvotes

--- src/ui/pie.js
+++ src/ui/pie.js
@@ -10,17 +10,24 @@ class Pie extends Infographic {
 
 var processed = JSON.parse(localStorage.getItem("redditDataJSON"));
 
+var data = [processed.upVotes, processed.downEst];
+var labels = ["Upvotes", "Downvotes"];
+if (processed.upVotes <= 0 && processed.downEst <= 0) {
+    data = [-1];
+    labels = ["(no votes)"];
+}
+
 var pie = new Pie(
     "pie",
-    "Upvotes/ Downvotes",
+    "Upvotes/Downvotes",
     400,
     400,
     [
         {
             name: "Upvotes/Downvotes",
-            labels: ["Upvotes","Downvotes"],
+            labels: labels,
             datasets: [{
-                data: [processed.upVotes, processed.downEst],
+                data: data,
                 backgroundColor: [
                     'rgb(82,150,221)',
                     'rgb(255,99,20)'
