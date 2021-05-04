1a804c7a2e0a0387c8a377a413d1a3273f7da793
Tim Lane <timlanesoftware@gmail.com>
2021-05-04 18:05:14 +0100

    Added JSON export functionality with alert warning if processing is incomplete

--- src/ui/generate_html.js
+++ src/ui/generate_html.js
@@ -11,17 +11,15 @@ function loadMeta() {
     titleDate.innerHTML = "<p style=\"font-size: 24pt;\">Analysis of <strong>" +
     processed.title + "</strong></p>" +"<h3>Generated on " + date + "</h3";
 
-    post_date = new Date((processed.postDate*1000));
+    post_date = new Date((processed.postDate * 1000));
     post_date = post_date.toString();
     //  puts information in info div
-    info.innerHTML =
-    "<h2> Post Uploaded on " + post_date + "</h2>" +
-    "<br /><h2>Upvotes: " + processed.upVotes + "</h2>" +
-    "<h2>Downvotes (estimate): " + processed.downEst + "</h2>" +
-    "<h2>Number of Comments: " + processed.numComments + "</h2>" +
-    "<br /><h3>Total Awards: " + processed.totalAwards + "</h3>" +
-    "<h3>Number of CrossPosts:  " + processed.crossPosts + "</h3>" +
-    "<h3>Comments Controversiality: " + String(processed.contCount)+ "</h3>" ;
+    info.innerHTML = "Stages completion: " + JSON.stringify(processed.stages) +
+    "<h2>Post uploaded on " + post_date + "</h2>" +
+    "<h2>Subreddit: <a href=\"https://reddit.com/r/" + processed.subreddit + "/\" target=\"_blank\">" + processed.subreddit + "</a></h2>" +
+    "<h2>Total Awards: " + processed.totalAwards + "</h2>" +
+    "<h2>Number of CrossPosts:  " + processed.crossPosts + "</h2>" +
+    "<h2>Comments Controversiality: " + String(processed.contCount)+ "</h2>";
 }
 
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
